f5741414fba6fc45b43bcb6bf4cd7de5cee75f8f
Ibrahim Alfalah <ibrahim.alfalah22@gmail.com>
2024-05-29 16:50:21 +0300

    Add particles

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,10 @@
 var THEMEMASCOT = {};
 
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
 (function($) {
     "use strict";
 
